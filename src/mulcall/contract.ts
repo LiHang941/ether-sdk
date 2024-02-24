652414c1c25e0273c41f42c0fcfded44a9c2ca1f
lihang941 <1329075357@qq.com>
2024-02-24 17:30:59 +0800

    feat: wallet connect v2

--- src/mulcall/contract.ts
+++ src/mulcall/contract.ts
@@ -22,7 +22,7 @@ export class MulContract {
 
     this._abi = toFragment(abi);
 
-    this._functions = this._abi.filter((x) => x.type === 'function').map((x) => FunctionFragment.from(x));
+    this._functions = this._abi.filter(it=> it).filter((x) => x.type === 'function').map((x) => FunctionFragment.from(x));
     const callFunctions = this._functions.filter((x) => x.stateMutability === 'pure' || x.stateMutability === 'view');
 
     for (const callFunction of callFunctions) {
